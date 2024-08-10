a16572f9416771d90b1a68d9a35ada6abd82b8a7
harilet <mymaillet2.0@gmail.com>
2024-08-10 03:31:02 -0400

    lint update

--- service/routes.py
+++ service/routes.py
@@ -62,6 +62,7 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to LIST accounts ...
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     accounts = Account.all()
@@ -74,6 +75,7 @@ def list_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     account = Account.find(account_id)
@@ -88,6 +90,7 @@ def get_accounts(account_id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     account = Account.find(account_id)
@@ -109,6 +112,7 @@ def delete_accounts(account_id):
     if account:
         account.delete()
     return "", status.HTTP_204_NO_CONTENT
+    
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
